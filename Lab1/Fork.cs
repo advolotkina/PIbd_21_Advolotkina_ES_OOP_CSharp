edf480f557510acfc9d286e61e20ea1f62b20f45
advolotkina <31734118+advolotkina@users.noreply.github.com>
2017-10-14 11:56:02 +0400

    Add files via upload

---
+++ Lab1/Fork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULSTU_OOP_Csharp_HomeLab1
+{
+    class Fork
+    {
+        public void Break(Egg egg)
+        {
+            if (egg.Have_shell)
+            {
+                egg.Have_shell = false;
+            }
+        }
+    }
+}
