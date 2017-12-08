ec74bcb2c6e7167bd77be6da8a9ab8311822cc4f
advolotkina <31734118+advolotkina@users.noreply.github.com>
2017-12-08 17:29:18 +0400

    Add files via upload

---
+++ Lab2/ClassArray.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Collections.Generic;
+
+namespace ULSTU_OOP_SCharp_Lab3
+{
+    class ClassArray<T> where T : IAnimal
+    {
+        private Dictionary<int, T> places;
+        private T defaultValue;
+
+        private int maxCount;
+
+        public ClassArray(int sizes, T defVal)
+        {
+            defaultValue = defVal;
+            places = new Dictionary<int, T>();
+            maxCount = sizes;
+        }
+
+        public static int operator +(ClassArray<T> p, T fish)
+        {
+            if(p.places.Count == p.maxCount)
+            {
+                return -1;
+            }
+            for(int i = 0; i < p.places.Count; i++)
+            {
+                if (p.CheckFreePlace(i))
+                {
+                    p.places.Add(i, fish);
+                    return i;
+                }
+            }
+            p.places.Add(p.places.Count, fish);
+            return p.places.Count - 1;
+        }
+
+        public static T operator -(ClassArray<T> p, int index)
+        {
+            if (p.places.ContainsKey(index))
+            {
+                T fish = p.places[index];
+                p.places.Remove(index);
+                return fish;
+            }
+            return p.defaultValue;
+        }
+
+        private bool CheckFreePlace(int index)
+        {
+            return !places.ContainsKey(index);
+        }
+
+        public T this[int ind]
+        {
+            get
+            {
+                if (places.ContainsKey(ind))
+                {
+                    return places[ind];
+                }
+                return defaultValue;
+            }
+        }
+    }
+}
---
+++ Lab2/Ocean.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ULSTU_OOP_SCharp_Lab3
+{
+    class Ocean
+    {
+        ClassArray<IAnimal> ocean;
+
+        List<ClassArray<IAnimal>> oceanLevels;
+
+        int currentLevel;
+
+        int countPlaces = 20;
+
+        int placeSizeWidth = 130;
+
+        int placeSizeHeight = 100;
+
+        public int getCurrentLevel
+        {
+            get
+            {
+                return currentLevel;
+            }
+        }
+
+        public Ocean(int countLevels)
+        {
+            oceanLevels = new List<ClassArray<IAnimal>>();
+            for(int i = 0; i < countLevels;i++)
+            {
+                ocean = new ClassArray<IAnimal>(countPlaces, null);
+                oceanLevels.Add(ocean);
+            }
+            
+        }
+
+        public void LevelUp()
+        {
+            if(currentLevel + 1 < oceanLevels.Count)
+            {
+                currentLevel++;
+            }
+        }
+
+        public void LevelDown()
+        {
+            if (currentLevel > 0)
+            {
+                currentLevel--;
+            }
+        }
+
+        public int PutFishInOcean(IAnimal fish)
+        {
+            return oceanLevels[currentLevel] + fish;
+        }
+
+        public IAnimal GetFishFromOcean(int n)
+        {
+            return oceanLevels[currentLevel] - n;
+        }
+
+        private void DrawOcean(Graphics g)
+        {
+            Pen pen = new Pen(Color.Aqua, 3);
+            g.DrawRectangle(pen, 0, 0, 550, 550);
+            Brush brush = new SolidBrush(Color.Aquamarine);
+            g.FillRectangle(brush,0,0,550,550);
+            for (int i = 0; i < countPlaces; i++)
+            {
+                
+                g.DrawEllipse(pen,  (int)i / 5 * placeSizeWidth + 5, i % 5 * placeSizeHeight,placeSizeWidth,placeSizeHeight);
+                
+            }
+        }
+
+        public void Draw(Graphics g)
+        {
+            DrawOcean(g);
+            for(int i = 0; i < countPlaces; i++)
+            {
+                var fish = oceanLevels[currentLevel][i];
+                if (fish != null)
+                {
+                    fish.setPosition(15+(int)i/5*placeSizeWidth+5,i%5*placeSizeHeight+15);
+                    fish.draw(g);
+                }
+            }
+        }
+
+    }
+}
