924d178d0f9e31bac183ca539f371365ea034d88
advolotkina <31734118+advolotkina@users.noreply.github.com>
2017-12-08 17:14:00 +0400

    Add files via upload

---
+++ Lab2/Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ULSTU_OOP_SCharp_Lab3
+{
+    public partial class Form2 : Form
+    {
+        Ocean ocean;
+        public Form2()
+        {
+            InitializeComponent();
+            ocean = new Ocean();
+            Draw();
+        }
+     
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            ocean.Draw(gr, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var fish = new Shark(10, 10, 300, cd.Color);
+                int place = ocean.PutFishInOcean(fish);
+               
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog cdDop = new ColorDialog();
+                if (cdDop.ShowDialog() == DialogResult.OK)
+                {
+                    var fish = new TigerShark(10, 10, 300, cd.Color, cdDop.Color);
+                    int place = ocean.PutFishInOcean(fish);
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "" )
+            {
+                var fish = ocean.GetFishFromOcean(Convert.ToInt32(maskedTextBox1.Text));
+                if (fish != null)
+                {
+                Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                fish.setPosition(40, 20);
+                fish.draw(gr);
+                pictureBox2.Image = bmp;
+                Draw();
+                }
+                
+            }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab2/Ocean.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+
+namespace ULSTU_OOP_SCharp_Lab3
+{
+    class Ocean
+    {
+        ClassArray<IAnimal> ocean;
+
+        int countPlaces = 20;
+
+        int placeSizeWidth = 130;
+
+        int placeSizeHeight = 100;
+
+        public Ocean()
+        {
+            ocean = new ClassArray<IAnimal>(countPlaces, null);
+        }
+
+        public int PutFishInOcean(IAnimal fish)
+        {
+            return ocean + fish;
+        }
+
+        public IAnimal GetFishFromOcean(int n)
+        {
+            return ocean - n;
+        }
+
+        private void DrawOcean(Graphics g)
+        {
+            Pen pen = new Pen(Color.Aqua, 3);
+            g.DrawRectangle(pen, 0, 0, 550, 550);
+            Brush brush = new SolidBrush(Color.Aquamarine);
+            g.FillRectangle(brush,0,0,550,550);
+            for (int i = 0; i < countPlaces; i++)
+            {
+                
+                g.DrawEllipse(pen,  (int)i / 5 * placeSizeWidth + 5, i % 5 * placeSizeHeight,placeSizeWidth,placeSizeHeight);
+                
+            }
+        }
+
+        public void Draw(Graphics g, int width, int height)
+        {
+            DrawOcean(g);
+            for(int i = 0; i < countPlaces; i++)
+            {
+                var fish = ocean.getObject(i);
+                if (fish != null)
+                {
+                    fish.setPosition(15+(int)i/5*placeSizeWidth+5,i%5*placeSizeHeight+15);
+                    fish.draw(g);
+                }
+            }
+        }
+    }
+}
